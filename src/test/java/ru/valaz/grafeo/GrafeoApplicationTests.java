db59c71537b1897f49bba3311c183b889b2fad82
valaz <info@valaz.ru>
2019-04-14 17:15:45 +0300

    test fix

--- src/test/java/ru/valaz/grafeo/GrafeoApplicationTests.java
+++ src/test/java/ru/valaz/grafeo/GrafeoApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource(properties = {
-        "environment=local",
+        "environment=test",
 })
 public class GrafeoApplicationTests {
 
