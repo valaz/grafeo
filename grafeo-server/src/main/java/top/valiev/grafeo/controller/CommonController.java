bc3d56a64d990162137e1b87343cacce538f308f
valaz <azat.vlv@gmail.com>
2019-05-08 01:54:55 +0300

    added health check api for server

--- grafeo-server/src/main/java/top/valiev/grafeo/controller/CommonController.java
+++ grafeo-server/src/main/java/top/valiev/grafeo/controller/CommonController.java
@@ -1,10 +1,13 @@
 package top.valiev.grafeo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.valiev.grafeo.payload.GaUid;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/common")
@@ -13,8 +16,19 @@ public class CommonController {
     @Value("${ga:GAUID}")
     private String gaUid;
 
+    private LocalDate startDate = LocalDate.now();
+
     @GetMapping("/ga")
     public GaUid getGaUid() {
         return new GaUid(gaUid);
+    }
+
+    @GetMapping("/fresh")
+    public ResponseEntity isFresh() {
+        if (LocalDate.now().equals(startDate)) {
+            return ResponseEntity.ok().body("Started today");
+        } else {
+            return ResponseEntity.badRequest().body("Started on " + startDate);
+        }
     }
 }
