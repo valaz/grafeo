69aaad15e6906681ff9563d2b11084e86c841114
valaz <azat.vlv@gmail.com>
2019-05-08 00:35:40 +0300

    added deploy script for grafeo-dev-web

--- .travis/deploy-web.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git status
-git stash
-git checkout -f dev
-git rebase HEAD dev
-git push -f deploy-web monorepo:master
---
+++ .travis/deploy_grafeo-web.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git status
+git stash
+git push -f grafeo-web master
