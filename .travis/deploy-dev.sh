36752a7f148fa33f1e3e03fd0870ce2ebecc64e8
valaz <azat.vlv@gmail.com>
2019-05-10 01:29:59 +0300

    removed old deploy scripts

--- .travis/deploy-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git status
-git stash
-git checkout -f dev
-git rebase HEAD dev
-git push -f deploy-dev dev:master
--- .travis/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git status
-git stash
-git checkout -f master
-git rebase HEAD master
-git push -f deploy master
