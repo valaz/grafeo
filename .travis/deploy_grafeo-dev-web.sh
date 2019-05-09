fddf687d8d05f6aee8aeffa818677e3173c5eff9
valaz <azat.vlv@gmail.com>
2019-05-10 00:33:07 +0300

    travis deploy upgrade

--- .travis/deploy_grafeo-dev-web.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git remote add grafeo-dev-web dokku@dokku.valiev.top:grafeo-dev-web
-git status
-git stash
-git push -f grafeo-dev-web monorepo:master
--- .travis/deploy_grafeo-dev_all.sh
+++ .travis/deploy_grafeo-dev_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+git remote add grafeo-dev-web dokku@dokku.valiev.top:grafeo-dev-web
 git remote add grafeo-dev dokku@dokku.valiev.top:grafeo-dev
 git status
 git stash
+git push -f grafeo-dev-web monorepo:master
 git push -f grafeo-dev monorepo:master
